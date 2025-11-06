39b2ed83555c1f99fd47054272ff95801f5eef08
BakaRickyClariS <ricky860526@gmail.com>
2025-11-06 15:48:13 +0800

    feat: add Layout component and update Router and main files for routing integration

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,5 @@
-import { createBrowserRouter } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout';
 
 export const router = createBrowserRouter(
   [
