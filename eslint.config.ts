5aad62a1fb03d9f42353bfcc25c237f252b89ef5
BakaRickyClariS <ricky860526@gmail.com>
2025-11-06 15:48:21 +0800

    feat: enhance ESLint configuration by adding React Hooks and Refresh plugins

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,8 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import prettier from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 
@@ -14,6 +16,8 @@ export default [
     files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       '@typescript-eslint': tseslint.plugin,
       prettier: prettierPlugin,
     },
@@ -30,10 +34,19 @@ export default [
         console: 'readonly',
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       'react/display-name': 'warn',
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-refresh/only-export-components': 'warn',
 
       '@typescript-eslint/no-unused-vars': [
         'warn',
