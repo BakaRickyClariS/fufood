18caa13f0a369d5143ed4c9c766ae555cf775bb6
BakaRickyClariS <ricky860526@gmail.com>
2025-11-07 15:12:41 +0800

    feat: add base route and tsx file

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,27 +1,26 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
+import Dashboard from './routes/Dashboard';
+import AuthRoutes from './routes/Auth';
+import Inventory from './routes/Inventory';
+import Recipe from './routes/Recipe';
+import SettingsRoutes from './routes/Settings';
 
 export const router = createBrowserRouter(
   [
     {
       path: '/',
       element: <Layout />,
-      // children: [
-      //   { index: true, element: <Home /> },
-      //   {
-      //     path: 'login',
-      //     element: <Login />,
-      //     handle: { headerVariant: 'simple', footer: false },
-      //   },
-      //   {
-      //     path: 'sign-up',
-      //     element: <Signup />,
-      //     handle: { headerVariant: 'simple', footer: false },
-      //   },
-      // ],
+      children: [
+        { index: true, element: <Dashboard /> },
+        ...AuthRoutes,
+        { path: 'inventory', element: <Inventory /> },
+        { path: 'recipe', element: <Recipe /> },
+        ...SettingsRoutes,
+      ],
     },
   ],
   {
-    basename: '/fufood/', // ← 設定前綴
+    basename: '/fufood/',
   },
 );
