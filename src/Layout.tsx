1a7d6de762e8f3a11587dff669bcec5db845ffd7
BakaRickyClariS <ricky860526@gmail.com>
2025-11-15 14:31:55 +0800

    feat: add bg color switch

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,8 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import TopNav from './components/layout/TopNav';
 import BottomNav from './components/layout/BottomNav';
+import { useEffect } from 'react';
 
 const Layout = () => {
+  const location = useLocation();
+  const isDashboard = location.pathname === '/';
+  useEffect(() => {
+    if (isDashboard) {
+      document.body.classList.add('body-dashboard-bg');
+    } else {
+      document.body.classList.remove('body-dashboard-bg');
+    }
+    return () => {
+      document.body.classList.remove('body-dashboard-bg');
+    };
+  }, [isDashboard]);
   return (
     <>
       <TopNav />
