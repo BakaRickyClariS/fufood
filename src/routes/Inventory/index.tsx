af47f181e481641181837a492a01702061de73bf
BakaRickyClariS <ricky860526@gmail.com>
2025-11-18 17:59:20 +0800

    style: clean comment

--- src/routes/Inventory/index.tsx
+++ src/routes/Inventory/index.tsx
@@ -1,22 +1,8 @@
 import HeroCard from '@/components/layout/HeroCard';
 import TabsSection from '@/components/layout/inventory/TabsSection';
 import MemberList from '@/components/layout/MemberList';
-// import { useEffect } from 'react';
-// import { useLocation } from 'react-router-dom';
 
 const Inventory: React.FC = () => {
-  // const location = useLocation();
-  // const isMember = location.pathname === '/';
-  // useEffect(() => {
-  //   if (isMember) {
-  //     document.body.classList.add('body-dashboard-bg');
-  //   } else {
-  //     document.body.classList.remove('body-dashboard-bg');
-  //   }
-  //   return () => {
-  //     document.body.classList.remove('body-dashboard-bg');
-  //   };
-  // }, [isMember]);
   return (
     <>
       <HeroCard>
