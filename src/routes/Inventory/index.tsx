0860d76036b4d7ea7965f2af49d099b33bbf3b78
BakaRickyClariS <ricky860526@gmail.com>
2025-11-19 17:35:11 +0800

    style: add category page style

--- src/routes/Inventory/index.tsx
+++ src/routes/Inventory/index.tsx
@@ -1,8 +1,13 @@
 import HeroCard from '@/components/layout/HeroCard';
 import TabsSection from '@/components/layout/inventory/TabsSection';
 import MemberList from '@/components/layout/MemberList';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const Inventory: React.FC = () => {
+  const location = useLocation();
+  const isMainInventory = location.pathname === '/inventory';
+  useEffect(() => {}, [isMainInventory]);
   return (
     <>
       <HeroCard>
