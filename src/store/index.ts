3d50ea42aeeffa818567cafecde682bb9862c74d
BakaRickyClariS <ricky860526@gmail.com>
2026-01-04 18:01:54 +0800

    refactor(shared): update global store and animation hooks

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,6 +7,7 @@ import groupModalReducer from '@/modules/groups/store/groupModalSlice';
 import refrigeratorReducer from './slices/refrigeratorSlice';
 import shoppingListReducer from './slices/shoppingListSlice';
 import consumptionReducer from '@/modules/inventory/store/consumptionSlice';
+import recipeReducer from '@/modules/recipe/store/recipeSlice';
 
 export const store = configureStore({
   reducer: {
@@ -18,6 +19,7 @@ export const store = configureStore({
     refrigerator: refrigeratorReducer,
     shoppingList: shoppingListReducer,
     consumption: consumptionReducer,
+    recipe: recipeReducer,
   },
 });
 
