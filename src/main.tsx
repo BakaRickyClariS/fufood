505921a9b8b15bbeadde857adfd5cd61bd53687d
BakaRickyClariS <ricky860526@gmail.com>
2026-01-05 13:00:41 +0800

    refactor(toast): 移除自定義 ToastContext 並遷移至 sonner
    1. 移除 ToastProvider
    2. 更新相機掃描元件反饋行為
    3. 完全統一使用 sonner 體系

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { store } from '@/store';
 import './styles/index.css';
 import App from '@/App';
-import { ToastProvider } from '@/shared/contexts/ToastContext';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/lib/queryClient';
 import { FCMProvider } from '@/shared/providers/FCMProvider';
@@ -13,11 +12,9 @@ createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <ToastProvider>
-          <FCMProvider autoRequest>
-            <App />
-          </FCMProvider>
-        </ToastProvider>
+        <FCMProvider autoRequest>
+          <App />
+        </FCMProvider>
       </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
